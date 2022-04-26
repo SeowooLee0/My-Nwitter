965d1929d5ef8447960d5dac37576cc9f9fb404d
goops2000 <goops2000@naver.com>
2022-04-27 00:12:03 +0900

    getTweet API로 tweetdata가져오기

--- src/routers/Home.tsx
+++ src/routers/Home.tsx
@@ -1,46 +1,48 @@
-import { signOut } from "firebase/auth";
-import React, { useState } from "react";
-import { auth } from "../firebase";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
 import "./Home.css";
-import // addDoc,
-// collection,
-// doc,
-// getDoc,
-// getDocs,
-// setDoc,
-"firebase/firestore";
-// import { db } from "../firebase";
 
 function Home() {
-  const onLogout = () => {
-    signOut(auth);
-  };
   const onSubmit = (event: any) => {
     event.preventDefault();
   };
+
+  useEffect(() => {
+    getTweets();
+  });
+
+  interface Tweet {
+    id: number;
+    number: string;
+    content: string;
+    write_date: string;
+  }
+
+  const getTweets = async () => {
+    axios({
+      method: "get",
+      url: "http://localhost:1234/getTweets",
+    }).then((res) => {
+      setData(res.data);
+    });
+  };
+
   const [tweet, setTweet] = useState("");
 
-  const [data, setData] = useState(["트윗을 입력하세요"]);
+  const [data, setData] = useState<Tweet[]>([]);
 
   const onClick = (event: any) => {
-    const { value } = event.target;
-    newData();
-    console.log(value);
+    // const newArray = [...data];
+    // newArray.push(tweet);
+    // setData(newArray);
   };
-  function newData() {
-    const newArray = [...data];
-    newArray.push(tweet);
-    setData(newArray);
-  }
 
   const onChange = (event: any) => {
     const { value } = event.target;
-    console.log(value.length);
     setTweet(value);
     if (value.length >= 6) {
-      console.log(value.length);
       alert("글자수는 5자리로 제한되어있습니다");
-
       const text = value.slice(0, 5);
       setTweet(text);
     }
@@ -63,10 +65,10 @@ function Home() {
           업로드
         </button>
         <div className="tweetBox">
-          {data.map((t, i) => {
+          {data.map((t) => {
             return (
-              <div className="tweet" key={i}>
-                {t}
+              <div className="tweet" key={t.id}>
+                {t.content}
               </div>
             );
           })}
