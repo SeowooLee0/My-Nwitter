02f15c72f8800d491678541774375a39bd6ba7ff
goops2000 <goops2000@naver.com>
2022-05-06 02:22:04 +0900

    Update Auth.tsx
    useRef 지우기

--- src/routers/Auth.tsx
+++ src/routers/Auth.tsx
@@ -1,13 +1,53 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import axios from "axios";
 
 function Auth() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onChange = (event: any) => {
+    const { name, value } = event.target;
+
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+  const onClick = (e: any) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:1234/register", {
+        email: email,
+        password: password,
+      })
+      .then((res) => {
+        alert("회원가입 성공");
+      })
+      .catch((res) => {
+        if (res.response.status === 400) {
+          alert(res.response.data.message);
+        }
+      });
+  };
+
   return (
     <>
       <div>Auth</div>
       <form>
-        <input placeholder="ID" />
-        <input placeholder="PW" />
-        <button>Login</button>
+        <input
+          placeholder="EMAIL"
+          name="email"
+          value={email}
+          onChange={onChange}
+        />
+        <input
+          placeholder="PW"
+          name="password"
+          value={password}
+          onChange={onChange}
+        />
+        <button onClick={onClick}>Login</button>
       </form>
     </>
   );
