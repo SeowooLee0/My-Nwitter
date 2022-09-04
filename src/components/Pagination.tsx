bc579c84e83ec23e9761d06b819d529bde32f5ab
goops2000 <goops2000@naver.com>
2022-09-04 23:13:37 +0900

    tailwind, scss, heartButton, pagination 추가

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,53 @@
+/* This example requires Tailwind CSS v2.0+ */
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/20/solid";
+
+import React, { useState } from "react";
+
+const Pagination = ({ postsPerPage, totalPosts, paginate }: any) => {
+  console.log(postsPerPage, totalPosts, paginate);
+  const pageNumber = [];
+
+  for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+    pageNumber.push(i);
+  }
+
+  return (
+    <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
+      <div className="flex flex-1 justify-between">
+        <nav
+          className="isolate inline-flex -space-x-px rounded-md shadow-sm"
+          aria-label="Pagination"
+        >
+          <a
+            href="3"
+            className="relative inline-flex items-center rounded-l-md border border-gray-300 bg-white px-2 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20"
+          >
+            <span className="sr-only">Previous</span>
+            <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
+          </a>
+
+          {pageNumber.map((number) => (
+            <div
+              aria-current="page"
+              className="relative inline-flex items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20"
+              key={number}
+              onClick={() => {
+                paginate(number);
+              }}
+            >
+              {number}
+            </div>
+          ))}
+          <a
+            href="2"
+            className="relative inline-flex items-center rounded-r-md border border-gray-300 bg-white px-2 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20"
+          >
+            <span className="sr-only">Next</span>
+            <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
+          </a>
+        </nav>
+      </div>
+    </div>
+  );
+};
+export default Pagination;
---
+++ src/components/TweetBox.tsx
@@ -0,0 +1,206 @@
+import axios from "axios";
+
+import React, { FC, useState } from "react";
+import { Link } from "react-router-dom";
+import { DataProps, Tweet } from "../routers/Tweets";
+import { HeartIcon } from "@heroicons/react/24/outline";
+
+const TweetBox = ({ data, id }: { data: Array<Tweet>; id: any }) => {
+  console.log(id);
+
+  const saveTweets = async () => {
+    axios
+      .post("http://localhost:1234/saveTweets", {
+        content: tweet,
+        tag: saveTag,
+      })
+      .then((res) => {
+        console.log(data);
+        if (res.data === "login again") {
+          alert("로그인이 만료되었습니다");
+        }
+      });
+  };
+
+  // const onLogout = () => {
+  //   axios.get("http://localhost:1234/logout");
+  //   alert("로그아웃 되었습니다");
+  //   window.location.reload();
+  // };
+
+  const [tweet, setTweet] = useState([]);
+  const [comment, setComment] = useState("");
+  const [tweetId, setTweetId] = useState("");
+  const [saveTag, setSaveTag] = useState("");
+  const [like, setLike] = useState(false);
+  const [login, setLogin] = useState(true);
+
+  const onClick = (event: any) => {
+    saveTweets();
+  };
+
+  const onChange = (event: any) => {
+    const { value } = event.target;
+
+    setTweet(value);
+    if (value.length >= 101) {
+      alert("글자수는 10자리로 제한되어있습니다");
+      const text = value.slice(0, 100);
+      setTweet(text);
+    }
+  };
+
+  const onTag = (event: any) => {
+    const { value } = event.target;
+    setSaveTag(value.match(/(#[^\s#]+)/g));
+  };
+
+  const onLogin = () => {
+    axios
+      .get("http://localhost:1234/refreshTokenRequest")
+      .then((res) => {
+        setGetId(res.data.email);
+        if (res.data.data === null) {
+          alert("로그인이 만료되었습니다");
+          window.location.reload();
+        }
+      })
+      .catch((err) => {});
+  };
+
+  const [getid, setGetId] = useState("");
+  const [check, setCheck] = useState(false);
+  //   const checkData = data.filter((data: Tweet) => data.email === id);
+
+  const onComment = (event: any) => {
+    setComment(event.target.value);
+    setTweetId(event.target.id);
+    console.log(comment);
+  };
+
+  const saveComment = (event: any) => {
+    event.preventDefault();
+    axios
+      .post("http://localhost:1234/saveComments", {
+        comment: comment,
+        tweet_id: tweetId,
+      })
+      .then((res) => {
+        console.log(res.data);
+        if (res.data === "login again") {
+          alert("로그인이 만료되었습니다");
+        }
+      });
+  };
+
+  const onCheck = (e: any) => {
+    if (e.target.checked === true) {
+      setCheck(true);
+    } else if (e.target.checked === false) {
+      setCheck(false);
+    }
+  };
+
+  const onHeartButton = () => {
+    setLike((prev) => !prev);
+  };
+
+  const checkData = data.filter((data) => data.email === id);
+  console.log(like);
+  return (
+    <>
+      <form className="form">
+        <input
+          className="text"
+          placeholder="트윗 입력란"
+          value={tweet}
+          onClick={onLogin}
+          onChange={onChange}
+          disabled={login ? false : true}
+        />
+        <input
+          className="text"
+          placeholder="태그 입력란"
+          onClick={onLogin}
+          onChange={onTag}
+        />
+        <button className="inputBtn" onClick={onClick}>
+          업로드
+        </button>
+        <input type="checkbox" value={id} onChange={onCheck} />
+        <div className="tweetBox">
+          {(check ? checkData : data).map((t: any) => {
+            return (
+              <>
+                <div className="tweet" key={t.tweet_id}>
+                  <p>작성자 : {t.email}</p>
+                  <p>{t.content}</p>
+                  <p>
+                    {t.tag === null
+                      ? ""
+                      : t.tag.map((tagId: any, i: any) => {
+                          const tagName = tagId.replace(/#/g, "");
+                          return (
+                            <>
+                              <span>#</span>
+                              <Link to={`/tag/${tagName}`} key={i}>
+                                {tagName}
+                              </Link>
+                            </>
+                          );
+                        })}
+                  </p>
+
+                  {/* {t.comments.map.comments} */}
+
+                  <div className="comment_inputBox">
+                    <input
+                      className="comment_input"
+                      id={`${t.tweet_id}`}
+                      type="text"
+                      placeholder="댓글달기..."
+                      onChange={onComment}
+                      // value={comment}
+                    />
+                    <button className="comment_button" onClick={saveComment}>
+                      게시
+                    </button>
+                  </div>
+                </div>
+                <div className="  flex">
+                  <img
+                    className="w-5 h-5 "
+                    alt="#"
+                    src={like ? "/assets/heart.png" : "/assets/EmptyHeart.png"}
+                    onClick={onHeartButton}
+                  />
+                  <img
+                    className="w-5 h-5"
+                    alt="#"
+                    src={"/assets/messenger.png"}
+                    // onClick={onHeartButton}
+                  />
+                </div>
+                <div className="commentBox">
+                  <div className="comment_title">Comments</div>
+                  {t.comment.map((t: any) => {
+                    return (
+                      <>
+                        <div key={t.id} className="comment">
+                          {`작성자 : ${t.email} ${t.comment}`}
+                        </div>
+                      </>
+                    );
+                  })}
+                </div>
+              </>
+            );
+            // }
+          })}
+        </div>
+      </form>
+    </>
+  );
+};
+
+export default TweetBox;
