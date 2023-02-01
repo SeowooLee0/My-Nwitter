9379ff39639ad7a4507e8070627150aff06a9953
goops2000 <goops2000@naver.com>
2023-02-01 21:51:58 +0900

    fix: axios intercepter header 엑세스토큰 쿠키 실시간반영

--- src/api/CommonAxios.ts
+++ src/api/CommonAxios.ts
@@ -1,21 +1,21 @@
 import axios, { AxiosInstance } from "axios";
 import { request } from "http";
-import cookie from "react-cookies";
+
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import router from "../router";
 import { changeState } from "../redux/createSlice/IsLoginSlice";
 import store, { RootState } from "../redux/store";
+import cookie from "react-cookies";
+import { useEffect } from "react";
 
 axios.defaults.withCredentials = true;
 
-let accessToken = cookie.load("accessToken");
-
 const customAxios = axios.create({
   baseURL: "http://localhost:1234/",
-  headers: {
-    Authorization: `Bearer ${accessToken}`,
-  },
+  // headers: {
+  //   Authorization: `Bearer ${accessToken}`,
+  // },
   // headers: { "Content-Type": "application/json" },
   withCredentials: true,
   timeout: 10000,
@@ -25,10 +25,15 @@ const { dispatch } = store;
 
 customAxios.interceptors.request.use(
   function (config) {
+    let accessToken = cookie.load("accessToken");
+    if (accessToken) {
+      console.log(accessToken);
+      config.headers.common["Authorization"] = `Bearer ${accessToken}`;
+    }
+    return config;
+
     // 요청 바로 직전
     // axios 설정값에 대해 작성합니다.
-
-    return config;
   },
   function (error) {
     console.log(error);
@@ -39,27 +44,42 @@ customAxios.interceptors.request.use(
 
 customAxios.interceptors.response.use(
   function (response) {
+    console.log(response.data);
     if (
       // 번호값으로 체크, 문자열 체크는 버그가 발생할 , 코드번호부여
       response.data.message === "invalid refresh token, please log in again"
     ) {
-      alert("로그인이 만료되었습니다");
+      // alert("로그인이 만료되었습니다");
       console.log("반응");
       dispatch(changeState(false));
+    }
+
+    if (response.status === 200) {
+      console.log("정상");
+
+      // const navigate = useNavigate();
+      // navigate("/");
     }
 
     return response;
   },
 
-  function (error) {
-    console.log(error);
-    if (error.response.staus === 200) {
-      console.log("성공");
-    }
-    if (error.response.staus === 500) {
-      const navigate = useNavigate();
-      navigate("/");
-    }
+  function (error: any) {
+    console.log(error.response);
+
+    // if (error.response.status === 419) {
+    //   console.log("accessToken 만료");
+    // }
+    // if (error.response.status === 200) {
+    //   console.log("성공");
+    // }
+    // if (error.response.status === 500) {
+    //   console.log("로그인이 만료되었습니다");
+    //   alert("재로그인 필요");
+    //   dispatch(changeState(false));
+    //   const navigate = useNavigate();
+    //   navigate("/");
+    // }
     // if (error.data === "login again") {
     //   const dispatch = useDispatch();
     //   alert("로그인이 만료되었습니다");
