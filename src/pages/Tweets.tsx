dfa834e8ba23c99331bab8eedf6c8a670b8fd6a6
goops2000 <goops2000@naver.com>
2023-04-03 22:50:56 +0900

    Feat: tweet작성 박스 분리

--- src/pages/Tweets.tsx
+++ src/pages/Tweets.tsx
@@ -16,7 +16,7 @@ import Pagination from "../components/layouts/Pagination";
 import Header from "../components/layouts/Header";
 import TweetBox from "../components/tweets/TweetBox";
 
-import CommentsList from "../components/tweets/commentList";
+import CommentsList from "../components/tweets/CommentList";
 import customAxios from "../api/CommonAxios";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../redux/store";
@@ -36,6 +36,7 @@ import SidebarRight from "../components/layouts/SidebarRight";
 import { save } from "react-cookies";
 import { useMutation, useQuery, useQueryClient } from "react-query";
 import { count } from "console";
+import AddTweet from "../components/tweets/AddTweet";
 
 export interface DataProps {
   data: Array<Tweet>;
@@ -63,6 +64,8 @@ export interface Data {
   comment: Array<Comment>;
   like: Array<Like>;
   is_opened: boolean;
+  retweet_opened: boolean;
+  is_bookmark: boolean;
   user_id: number;
   is_like: Array<isLike>;
 }
@@ -110,7 +113,7 @@ interface saveTweets {
 }
 
 const Tweets = () => {
-  const data1 = useSelector((state: RootState) => state.getData.currentPosts);
+  // const data1 = useSelector((state: RootState) => state.getData.currentPosts);
   const target = useRef<any>(null);
   const id = useSelector((state: RootState) => state.getData.id);
   const isLoaded = useSelector((state: RootState) => state.getData.isLoaded);
@@ -141,7 +144,6 @@ const Tweets = () => {
       } else {
         setAddData([...res.data.data]);
       }
-      console.log(res.data);
 
       dispatch(
         changGetDataState({
@@ -187,7 +189,7 @@ const Tweets = () => {
     return () => {
       observer.disconnect();
     };
-  }, [getTotalPageNumber, target]);
+  }, [target]);
 
   useEffect(() => {
     socket.on("RECEIVE_COMMENT", (data: any) => {
@@ -202,160 +204,23 @@ const Tweets = () => {
     // };
   }, [socket]);
 
-  // const [data, setData] = useState<Tweet[]>([]);
+  return (
+    <>
+      {/* <Header /> */}
 
-  // const currentPost = data.slice(0, 10);
-  const [tweet, setTweet] = useState("");
-  const [saveTag, setSaveTag] = useState("");
-  {
-    const updateTweet = useMutation(
-      (newData: saveTweets) =>
-        customAxios.post("/saveTweets", newData).then((res: any) => {
-          console.log(res);
-        }),
-      {
-        onSuccess: () => {
-          console.log("onSuccess");
-          // queryClient.invalidateQueries(["select"]); // queryKey 유효성 제거
-        },
-        onError: (res) => {
-          console.log(res);
-          // queryClient.invalidateQueries(["select"]); // queryKey 유효성 제거
-        },
-      }
-    );
+      <div className=" flex">
+        <Sidebar />
 
-    // const onClick = (event: any) => {
-    //   saveTweets();
-    //   event.preventDefault();
-    // };
-
-    const onChange = (event: any) => {
-      const { value } = event.target;
-
-      setTweet(value);
-      if (value.length >= 101) {
-        alert("글자수는 10자리로 제한되어있습니다");
-        const text = value.slice(0, 100);
-        setTweet(text);
-      }
-    };
-
-    const onTag = (event: any) => {
-      const { value } = event.target;
-      setSaveTag(value.match(/(#[^\s#]+)/g));
-    };
-
-    const onLogin = () => {
-      customAxios
-        .get("/refreshTokenRequest")
-        .then((res) => {
-          // if (res.data.data === null) {
-          //   alert("로그인이 만료되었습니다");
-          // }
-        })
-        .catch((err) => {});
-    };
-
-    const [file, setFile] = useState("");
-
-    const onFileChange = (e: any) => {
-      setFile(e.target.files[0]);
-      console.log(e.target.files[0]);
-    };
-
-    const onUpload = (e: any) => {
-      e.preventDefault();
-      const formData = new FormData();
-
-      formData.append("upload_file", file);
-      formData.append("id", id);
-      formData.append("tweet", tweet);
-      formData.append("tag", saveTag);
-
-      console.log(formData, tweet, saveTag);
-      axios
-        .post("http://localhost:1234/upload/tweets", formData, {
-          headers: {
-            "content-type": "multipart/form-data",
-          },
-        })
-        .then((res) => {
-          console.log({ res });
-        })
-        .catch((res) => {
-          console.log({ res });
-        });
-    };
-
-    return (
-      <>
-        {/* <Header /> */}
-
-        <div className=" flex">
-          <Sidebar />
-          <div className="middleBox flex-col grow">
-            <form>
-              <div className="tweetTop">
-                <div className="title">Home</div>
-                <div className="flex p-5 tweetWritingBox ">
-                  <img
-                    className="w-8 h-8 pt-0 m-1"
-                    alt="#"
-                    src={"/assets/user(1).png"}
-                  />
-                  <div className="w-full">
-                    <input
-                      className="input"
-                      name="tweet"
-                      placeholder="What's happening?"
-                      value={tweet}
-                      onClick={onLogin}
-                      onChange={onChange}
-                    />
-                    <input
-                      className="input"
-                      name="tag"
-                      placeholder="#태그"
-                      onClick={onLogin}
-                      onChange={onTag}
-                    />
-                    <input
-                      name="upload_file"
-                      type="file"
-                      accept="*"
-                      onChange={onFileChange}
-                      placeholder="업로드"
-                    />
-                    <div className="inputBtn">
-                      <button
-                        onClick={
-                          file
-                            ? onUpload
-                            : (e: any) => {
-                                e.preventDefault();
-                                updateTweet.mutate({
-                                  content: tweet,
-                                  tag: saveTag,
-                                });
-                              }
-                        }
-                      >
-                        업로드
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <TweetBox data={addData} />
-            </form>
-          </div>
-          <SidebarRight />
+        <div className="middleBox flex-col grow">
+          <div className="title">Home</div>
+          <AddTweet />
+          <TweetBox data={addData} />
         </div>
-        <div ref={target}>{isLoaded && <p>Loading...</p>}</div>
-      </>
-    );
-  }
+        <SidebarRight />
+      </div>
+      <div ref={target}>{isLoaded && <p>Loading...</p>}</div>
+    </>
+  );
 };
 
 export default Tweets;
